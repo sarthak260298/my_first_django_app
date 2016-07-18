1828b143ec63629d36664c9cdef2ca196e31e5df
Your Name <you@example.com>
2016-07-18 16:20:19 +0530

    add wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
+from dj_static import Cling
 application = get_wsgi_application()
